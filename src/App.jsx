1ec3a0fa17121b213463873444b29801b88c4659
viktoriagrubi <wiktoria.grubizna@op.pl>
2025-09-29 21:20:43 +0200

    appearance improvement

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Header from "./components/Header/Header";
 import Dashboard from "./components/Dashboard/Dashboard";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div>
+    <div className={styles.app}>
       <Header />
       <Dashboard />
     </div>
